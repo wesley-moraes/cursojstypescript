10347dde4e67965c12f6b9f79da40f648e87bedf
wesley-moraes <wesleycrz@hotmail.com>
2024-09-20 17:41:44 -0300

    type enum

---
+++ src/A009-type-enum/A009-type-enum.ts
@@ -0,0 +1,29 @@
+enum Cores{
+    vermelho, //0
+    azul, //1
+    amarelo //2
+}
+
+console.log(Cores);
+console.log(Cores.vermelho);
+console.log(Cores[0]);
+
+//Eu posso criar mais enums e ele vai juntar as variaveis
+
+enum Cores{
+    verde = "verde",
+    rosa = 10,
+    purpura
+}
+
+console.log(" ");
+console.log(Cores);
+
+function escolhaCor(cor: Cores): void{
+    console.log(Cores[cor]);
+}
+
+console.log(" ");
+console.log(" ");
+console.log(Cores.vermelho);
+console.log(Cores[11]);
