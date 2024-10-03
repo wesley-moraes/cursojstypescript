b9a5ce7a89919f2d22611eeab37e47d9d0de829d
wesley-moraes <wesleycrz@hotmail.com>
2024-10-03 17:33:09 -0300

    webpack config #2

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//console.log('olá')
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mod_1 = __importDefault(require("./mod"));
+(0, mod_1.default)();
